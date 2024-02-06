68de71df3dae3f7f7ee177ebfd49f2844a7e11f7
PlotChat <levietanh1234567@gmail.com>
2024-02-06 17:46:46 +0700

    small changes

---
+++ script.js
@@ -0,0 +1,37 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    dropsAmount = 10;
+} else{
+    dropsAmount = 25;
+}
+
+var container = document.getElementById("container");
+
+function currentWSize(){
+    WSize = window.innerWidth;
+    return WSize;
+}
+
+function removeRain(){
+    container.innerHTML = "";
+}
+
+function makeRain(){
+    removeRain();
+    setTimeout(() =>{
+        removeRain();
+        for (i=0;i<dropsAmount;i++){
+            drop = document.createElement("div");
+            drop.className = "drop"; 
+            drop.style.left = Math.floor(Math.random() * currentWSize()) + "px";
+    
+            drop.style.animationDelay = Math.random() + "s";
+            console.log(Math.random() + "s")   
+    
+            container.appendChild(drop);
+        }
+    },1000)
+}
+
+window.addEventListener("load", makeRain);
+window.addEventListener("resize", makeRain);
+window.addEventListener("beforeunload", removeRain);
