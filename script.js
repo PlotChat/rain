947509b5184f6d84649a2fee0dab369c8b346a1b
PlotChat <levietanh1234567@gmail.com>
2024-02-29 01:47:53 +0700

    big changes :)

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ var container = document.getElementById("container");
 var rainContainer = document.getElementById("rainContainer");
 var dropsContainer1 = document.getElementById("drops1");
 var dropsContainer2 = document.getElementById("drops2");
+var documentHeight = document.documentElement.style.setProperty('--document-height', `${document.documentElement.scrollHeight}px`);
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    dropsAmount = 20;
+    existingDrops = container.querySelectorAll(".drop");
+    existingDrops.forEach(drop => {
+        drop.style.width = "2px";
+        drop.style.height = "10px";
+    });
+
+} else{
+    dropsAmount = 60;
+}
+
 
 function currentWSize(){
     WSize = window.innerWidth;
@@ -50,14 +64,15 @@ window.addEventListener("resize", () => {
 });
 window.addEventListener("beforeunload", makeRain);
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    dropsAmount = 20;
-    existingDrops = container.querySelectorAll(".drop");
-    existingDrops.forEach(drop => {
-        drop.style.width = "2px";
-        drop.style.height = "10px";
-    });
+disableRainBtn = document.getElementById("disableRainBtn");
+disableRainBtn.addEventListener("click", () => {
+    if (disableRainBtn.classList.contains("rainEnabled")){
+        rainContainer.style.display = "none";
+        disableRainBtn.classList.remove("rainEnabled");
+    }
 
-} else{
-    dropsAmount = 60;
-}
+    else {
+        rainContainer.style.display = "block";
+        disableRainBtn.classList.add("rainEnabled");
+    }
+})
